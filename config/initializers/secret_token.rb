ffaa5aa80f96678c953c5f33c5ce7d6cb8bacb58
Aaron Wilson <aarowilso@gmail.com>
2013-05-23 13:01:20 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhosDown::Application.config.secret_token = 'bd2644f79b6349ab22e06625a72b5ff4117d697af7496fea986ca3897eadb3160404c11e54e6a0e520bfed94095cfce646c9c1d16c28964be13f0a21b750c707'
